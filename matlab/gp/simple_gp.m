6c164eb09d8d1886bab2bebdc006aced5490421a
avishais <avishai.sintov@gmail.com>
2018-09-07 00:21:42 -0400

    Closed-loop results

--- matlab/gp/simple_gp.m
+++ matlab/gp/simple_gp.m
@@ -10,23 +10,23 @@ px2mm = 0.2621;
 data_source = '20';
 test_num = 1;
 mode = 8;
-% w = [1.05 1.05 1 1 2 2 3 3]; % For cyl 25 and mode 8
-switch mode
-    case 1
-        w = [3 3 1 1];
-    case 2
-        w = [3 3 1 1 1 1 1 1];
-    case 3
-        w = [3 3 1 1 1 1 1 1 1 1 3 3];
-    case 4
-        w = [];
-    case 5
-        w = [60 60 1 1 3 3];
-    case 7
-        w = [10 10 ones(1,14)];
-    case 8
-        w = [];%[5 5 3 3 1 1 3 3];%[5 5 3 3 1 1 3 3]; % Last best: [3 3 1 1 1 1 1 1];
-end
+w = [];%1.05 1.05 1 1 2 2 3 3]; % For cyl 25 and mode 8
+% switch mode
+%     case 1
+%         w = [3 3 1 1];
+%     case 2
+%         w = [3 3 1 1 1 1 1 1];
+%     case 3
+%         w = [3 3 1 1 1 1 1 1 1 1 3 3];
+%     case 4
+%         w = [];
+%     case 5
+%         w = [60 60 1 1 3 3];
+%     case 7
+%         w = [10 10 ones(1,14)];
+%     case 8
+%         w = [];%[5 5 3 3 1 1 3 3];%[5 5 3 3 1 1 3 3]; % Last best: [3 3 1 1 1 1 1 1];
+% end
 [Xtraining, Xtest, kdtree, I] = load_data(mode, w, test_num, data_source);
 
 Sr = Xtest;
@@ -76,7 +76,7 @@ disp(toc)
 
 disp(['mse = ' num2str(MSE(SRI, SI) * px2mm)]);
 
-save(['./paths_solution_mats/pred_' data_source '_' num2str(mode) '_' num2str(test_num) '_dm.mat'],'data_source','I','loss','mode','S','SI','Sr','SRI','test_num','w','Xtest');
+save(['./paths_solution_mats/pred_' data_source '_' num2str(mode) '_' num2str(test_num) '.mat'],'data_source','I','loss','mode','S','SI','Sr','SRI','test_num','w','Xtest');
 
 %% Closed loop
 
---
+++ matlab/track_process/track_process_vs.m
@@ -0,0 +1,120 @@
+clear all
+clc
+
+M = dlmread('pt_vs_04.txt');
+
+im_start = 14;
+images_test_folder = '../../data/test_images/pt_vs_04/';
+file_prefix = 'image_test3_';
+file = [images_test_folder, file_prefix, num2str(im_start), '_*.jpg'];
+files = dir(fullfile(file));
+IM = imread([files.folder '/' files.name]);
+
+obj_pos = MovingAvgFilter(M(:,18:19));
+carrot_pos = M(:,end-1:end);
+
+carrot_pos(all(carrot_pos==0,2),:) = [];
+obj_pos(all(obj_pos==0,2),:) = [];
+
+carrot_pos = [obj_pos(1,:); carrot_pos; obj_pos(1,:)];
+
+figure(1)
+clf
+imshow(IM);
+hold on
+plot(carrot_pos(:,1),carrot_pos(:,2),'--b');
+plot(obj_pos(:,1),obj_pos(:,2),'-r');
+hold off
+axis equal
+
+%%
+record = 1;
+
+files = dir(fullfile(images_test_folder, '*.jpg'));
+files = struct2cell(files)';
+files = sortrows(files, 1);
+files = files(:,1);
+
+if record
+    writerObj = VideoWriter('/home/avishai/Dropbox/transfer/rec_vs_04.avi'); %my preferred format
+    writerObj.FrameRate = 60;
+    open(writerObj);
+end
+
+ix = im_start;
+speed = 5;
+for i = 1:speed:size(obj_pos,1)
+    
+    filename = find_file(file_prefix, ix, files);
+    
+    IM = imread([images_test_folder filename]);
+    
+    figure(1)
+    clf
+    imshow(IM);
+    hold on
+    plot(carrot_pos(:,1),carrot_pos(:,2),'-y','linewidth',2);
+    plot(obj_pos(1:i,1),obj_pos(1:i,2),'-r','linewidth',2);
+    hold off
+    
+    ix = ix + speed; 
+    
+    if record
+        frame = getframe(gcf); % 'gcf' can handle if you zoom in to take a movie.
+        frame.cdata = imcrop(frame.cdata, [360 90 770-360 420-90]);
+        frame.cdata = insertText(frame.cdata,[260 8],'visual servoing','fontsize',18);
+%         imshow(frame.cdata);
+        writeVideo(writerObj, frame);
+    end
+%     drawnow;
+end
+
+if record
+    close(writerObj); % Saves the movie.
+end
+
+
+
+
+%%
+function y = MovingAvgFilter(x, windowSize)
+
+if nargin==1
+    windowSize = 13;
+end
+
+y = x;
+
+w = floor(windowSize/2);
+for j = 1:size(x,2)
+    for i = w+1:size(x,1)-w-1
+        
+        y(i,j) = sum(x(i-w:i+w,j))/length(i-w:i+w);
+        
+    end
+    
+end
+end
+
+
+function filename = find_file(file_prefix, index, files)
+
+f = [file_prefix num2str(index)];
+
+for i = 1:size(files,1)
+    
+    str = files(i);
+    str = str{1};
+    j = 1;
+    while j <= length(f) && f(j)==str(j)
+        j = j + 1;
+    end
+    if str(j)=='_'
+        ix = i;
+        break;
+    end
+end
+filename = files(ix);
+filename = filename{1};
+    
+end
